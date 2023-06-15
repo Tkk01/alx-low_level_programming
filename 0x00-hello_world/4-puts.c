152e005dd1db16dff6fc543446e6dd57d35d7308
Tkk01 <thangwakimani22@gmail.com>
2023-06-15 15:54:33 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - it prints a message with puts function
- * return: 0 (Success)
+ * main - It prints a message with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"); 
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
