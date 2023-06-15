5c0d79fa3dea2a2d027189ba42150d9c6686692f
Tkk01 <thangwakimani22@gmail.com>
2023-06-15 15:38:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - it prints a message with puts function
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"); 
+	return (0);
+}
