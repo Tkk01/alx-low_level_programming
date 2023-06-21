6b68de0ea3c127d6ec35effcbe1f2021ed62ea09
Tkk01 <thangwakimani22@gmail.com>
2023-06-21 08:04:40 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the lowercase alphabet 10 times followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+	char alphabet[27] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(alphabet[j]);
+		}
+		_putchar('\n');
+	}
+}
